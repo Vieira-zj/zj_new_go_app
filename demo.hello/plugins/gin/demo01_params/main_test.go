74b4db9c09c4a88a28f2eb0ca60a9a0b99d8353a
zhengjin <zivieira@163.com>
2021-11-02 11:09:05 +0800

    Refactor gin demos.

--- demo.hello/plugins/gin/demo01_params/main_test.go
+++ demo.hello/plugins/gin/demo01_params/main_test.go
@@ -9,10 +9,10 @@ import (
 )
 
 func TestPingRoute(t *testing.T) {
-	router := setupRouter()
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/ping", nil)
+
+	router := setupRouter()
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
