6b9db0acbbf2851720bc1ceecb33364251a5f849
zhengjin <zivieira@163.com>
2022-08-11 18:40:42 +0800

    Add demos.

--- demo.go.new/go1_1711_demos/demos/go_test.go
+++ demo.go.new/go1_1711_demos/demos/go_test.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
 	"testing"
 	"time"
 )
@@ -46,6 +49,130 @@ func TestMarshalFunc(t *testing.T) {
 	}
 	fmt.Printf("caller: %s\n", b)
 }
+
+func TestStructToMap(t *testing.T) {
+	type person struct {
+		ID   uint8  `json:"id"`
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	p := person{
+		ID:   1,
+		Name: "foo",
+		Age:  31,
+	}
+	b, err := json.Marshal(&p)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m := make(map[string]interface{})
+	if err := json.Unmarshal(b, &m); err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("src map:", m)
+
+	// add key, value
+	m["comment"] = "for test"
+	// update key name
+	m["identity"] = m["id"]
+	delete(m, "id")
+
+	b, err = json.MarshalIndent(&m, "", "  ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("dst map:\n%s\n", b)
+}
+
+func TestContextWithValue(t *testing.T) {
+	type person struct {
+		name string
+		age  int
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	p := &person{
+		name: "foo",
+		age:  31,
+	}
+	newCtx := context.WithValue(ctx, "key", p)
+	time.Sleep(300 * time.Millisecond)
+
+	p, ok := newCtx.Value("key").(*person)
+	if !ok {
+		t.Fatal("type error")
+	}
+	t.Logf("name=%s, age=%d", p.name, p.age)
+}
+
+func TestContinueInSelect(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	tick := time.Tick(time.Second)
+	for i := 0; i < 10; i++ {
+		t.Logf("run at: %d", i)
+		select {
+		case <-ctx.Done():
+			t.Log("cancelled")
+		case <-tick:
+			if i%2 == 1 {
+				continue
+			}
+			t.Logf("select at: %d", i)
+		}
+	}
+	t.Log("done")
+}
+
+// Demo: get func name and run by reflect
+
+type caller func(string)
+
+func sayHello(name string) {
+	fmt.Println("Hello:", name)
+}
+
+func exec(c interface{}, params ...interface{}) {
+	typeOf := reflect.TypeOf(c)
+	fmt.Println("type:", typeOf.Kind())
+	if typeOf.Kind() != reflect.Func {
+		fmt.Println("not caller")
+		return
+	}
+
+	// get func name
+	valueOf := reflect.ValueOf(c)
+	name := runtime.FuncForPC(valueOf.Pointer()).Name()
+	pkgName, funcName := getFuncName(name)
+	fmt.Printf("exec: pkg=%s, func=%s()\n", pkgName, funcName)
+
+	// run func()
+	paramValues := make([]reflect.Value, 0, len(params))
+	for _, param := range params {
+		paramValues = append(paramValues, reflect.ValueOf(param))
+	}
+	valueOf.Call(paramValues)
+
+	_, ok := valueOf.Interface().(caller)
+	fmt.Println("is caller:", ok)
+}
+
+func getFuncName(fullName string) (pkgName, funcName string) {
+	items := strings.Split(fullName, ".")
+	return items[0], items[1]
+}
+
+func TestGetFuncNameByReflect(t *testing.T) {
+	exec(sayHello, "foo")
+	t.Log("done")
+}
+
+// Demo: goroutine
 
 func TestRunBatchByGoroutine(t *testing.T) {
 	resultCh := make(chan int)
