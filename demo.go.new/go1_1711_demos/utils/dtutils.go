428fb26d51d36ea3ecb7091d6a87c30c419cd126
zhengjin <zivieira@163.com>
2022-08-27 11:16:24 +0800

    Add datetime utils.

---
+++ demo.go.new/go1_1711_demos/utils/dtutils.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+const DefaultTimeFormat = "2006-01-02 15:04:05"
+
+// GetStartAndEndMilliOfDate dt must be "2006-01-02" but not "2006-1-2".
+func GetStartAndEndMilliOfDate(dt string) (int64, int64, error) {
+	date := fmt.Sprintf("%s 00:00:00", dt)
+	ti, err := time.ParseInLocation(DefaultTimeFormat, date, time.Local)
+	if err != nil {
+		return -1, -1, err
+	}
+	fmt.Println(ti)
+	return ti.UnixMilli(), ti.Add(24*time.Hour - time.Second).UnixMilli(), nil
+}
+
+func GetDatesInDuration(start, end string) ([]string, error) {
+	startTime, err := time.ParseInLocation(DefaultTimeFormat, fmt.Sprintf("%s 00:00:00", start), time.Local)
+	if err != nil {
+		return nil, err
+	}
+	endTime, err := time.ParseInLocation(DefaultTimeFormat, fmt.Sprintf("%s 00:00:00", end), time.Local)
+	if err != nil {
+		return nil, err
+	}
+
+	subDays := int(endTime.Sub(startTime).Hours() / 24)
+	retDates := make([]string, 0, subDays)
+	for i := 0; i <= subDays; i++ {
+		tmpTime := startTime.Add(time.Duration(i) * 24 * time.Hour)
+		retDates = append(retDates, GetSimpleDate(tmpTime))
+	}
+	return retDates, nil
+}
+
+func GetSimpleDate(t time.Time) string {
+	return fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+}
---
+++ demo.go.new/go1_1711_demos/utils/dtutils_test.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimeParse(t *testing.T) {
+	date := "2022-10-01 00:00:00"
+	ti, err := time.Parse(DefaultTimeFormat, date)
+	assert.NoError(t, err)
+	t.Log("utc:", ti)
+
+	ti, err = time.ParseInLocation(DefaultTimeFormat, date, time.Local)
+	assert.NoError(t, err)
+	t.Log("cst:", ti)
+}
+
+func TestGetStartAndEndMilliOfDate(t *testing.T) {
+	now := time.Now()
+	for _, dt := range []string{
+		"2022-10-01",
+		GetSimpleDate(now),
+	} {
+		start, end, err := GetStartAndEndMilliOfDate(dt)
+		assert.NoError(t, err)
+		startTime := time.UnixMilli(start)
+		endTime := time.UnixMilli(end)
+		t.Log("start:", start, startTime)
+		t.Log("end", end, endTime)
+	}
+}
+
+func TestGetDatesInDuration(t *testing.T) {
+	res, err := GetDatesInDuration("2022-08-27", "2022-09-03")
+	assert.NoError(t, err)
+	t.Log(res)
+}
