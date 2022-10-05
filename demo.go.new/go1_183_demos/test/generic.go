f3ebe5ddfcebb99432afc0c31d79f57a6912ea9c
zhengjin <zivieira@163.com>
2022-10-05 12:51:09 +0800

    Add generic demos.

--- demo.go.new/go1_183_demos/test/generic.go
+++ demo.go.new/go1_183_demos/test/generic.go
@@ -6,7 +6,33 @@ import (
 	"strconv"
 )
 
-// InsertAt .
+// AddByReflect add fn supports for int32 and float32 by reflect.
+func AddByReflect(a, b interface{}) (interface{}, error) {
+	// here use generic [T any] instead of interface{}
+	aType := reflect.TypeOf(a).Name()
+	bType := reflect.TypeOf(b).Name()
+	fmt.Printf("param type is: %s, %s\n", aType, bType)
+	if aType != bType {
+		return -1, fmt.Errorf("param a and b type are mismatch")
+	}
+
+	aValueOf := reflect.ValueOf(a)
+	bValueOf := reflect.ValueOf(b)
+	switch aValueOf.Kind() {
+	case reflect.Int32:
+		return aValueOf.Int() + bValueOf.Int(), nil
+	case reflect.Float32:
+		return aValueOf.Float() + bValueOf.Float(), nil
+	default:
+		return -1, fmt.Errorf("param must be int32 or float32")
+	}
+}
+
+// AddByGeneric add fn supports for int32 and float32 by generic.
+func AddByGeneric[T int32 | float32](a, b T) T {
+	return a + b
+}
+
 func InsertAt[T any](list []T, idx int, t T) ([]T, error) {
 	l := len(list)
 	if idx < 0 {
@@ -26,7 +52,6 @@ func InsertAt[T any](list []T, idx int, t T) ([]T, error) {
 	return retList, nil
 }
 
-// RemoveAt .
 func RemoveAt[T any](list []T, idx int, t T) ([]T, error) {
 	l := len(list)
 	if idx < 0 {
@@ -45,7 +70,6 @@ func RemoveAt[T any](list []T, idx int, t T) ([]T, error) {
 	return retList, nil
 }
 
-// GetFieldInfo .
 func GetFieldInfo[T any /* int | string */](field T) string {
 	typeOf := reflect.TypeOf(field)
 	valueOf := reflect.ValueOf(field)
@@ -53,10 +77,7 @@ func GetFieldInfo[T any /* int | string */](field T) string {
 		valueOf = valueOf.Elem()
 	}
 
-	var (
-		fType  = ""
-		fValue = ""
-	)
+	var fType, fValue string
 	switch valueOf.Kind() {
 	case reflect.Int:
 		fType = "int"
--- demo.go.new/go1_183_demos/test/generic_test.go
+++ demo.go.new/go1_183_demos/test/generic_test.go
@@ -5,6 +5,35 @@ import (
 	"testing"
 )
 
+func TestAddByReflect(t *testing.T) {
+	res, err := AddByReflect(1, 1.0)
+	t.Logf("result=%d, error=%s", res, err)
+
+	res, err = AddByReflect(int32(1), int32(2))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("result:", res)
+
+	res, err = AddByReflect(float32(1.1), float32(2.2))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("result: %.2f", res)
+}
+
+func TestAddByGeneric(t *testing.T) {
+	res1 := AddByGeneric(int32(1), 1)
+	t.Log("result:", res1)
+
+	res2 := AddByGeneric(float32(1.0), 2.1)
+	t.Log("result:", res2)
+
+	// error
+	// res3 := AddByGeneric(float32(1.0), int32(1))
+	// t.Log("result:", res3)
+}
+
 type SampleSlice[T any] []T
 
 func TestGenericSlice(t *testing.T) {
