42537de191083843eb8f462745522811f164ae2b
zhengjin <zivieira@163.com>
2023-02-05 15:52:13 +0800

    Update json demos.

--- demo.go.new/go1_1711_demos/demos/json_test.go
+++ demo.go.new/go1_1711_demos/demos/json_test.go
@@ -7,8 +7,10 @@ import (
 	"go1_1711_demo/utils"
 	"io"
 	"reflect"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/imdario/mergo"
 	"github.com/stretchr/testify/assert"
@@ -22,6 +24,23 @@ func TestJsonValid(t *testing.T) {
 	str = `"name":"foo"}`
 	ok = json.Valid([]byte(str))
 	t.Log("is valid:", ok)
+}
+
+func TestUnmarshalToInterface(t *testing.T) {
+	type person struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	var p interface{}
+	p = person{}
+	err := json.Unmarshal([]byte(`{"name":"foo","age":40}`), &p)
+	assert.NoError(t, err)
+	t.Logf("person: %+v, kind:%s", p, reflect.TypeOf(p).Kind().String())
+
+	m, ok := p.(map[string]interface{})
+	assert.True(t, ok)
+	t.Logf("name=%v, age=%v", m["name"], m["age"])
 }
 
 func TestMarshalFunc(t *testing.T) {
@@ -42,7 +61,7 @@ func TestMarshalFunc(t *testing.T) {
 	fmt.Printf("caller: %s\n", b)
 }
 
-func TestMarshalWithType(t *testing.T) {
+func TestMarshalWithString(t *testing.T) {
 	type data struct {
 		Id  int64 `json:"id,string"`
 		Age int   `json:"age,string"`
@@ -94,39 +113,61 @@ func TestMarshalZeroValueOfStruct(t *testing.T) {
 		Name string `json:"name"`
 	}
 
+	// #1
 	s := Student{Name: "foo"}
 	b, err := json.Marshal(&s)
 	assert.NoError(t, err)
-	t.Logf("student: %s", b)
+	t.Logf("marshal: %s", b)
 
+	// ID 序列化为默认零值
+	s = Student{}
+	json.Unmarshal(b, &s)
+	assert.NoError(t, err)
+	t.Logf("unmarshal: %+v", s)
+
+	// #2
+	s = Student{Name: "bar"}
 	result, err := utils.JsonDumps(s)
 	assert.NoError(t, err)
 	t.Logf("dumps: %s", result)
 
-	// ID 序列化为默认零值
 	s = Student{}
 	err = utils.JsonLoads(result, &s)
 	assert.NoError(t, err)
 	t.Logf("loads: %+v", s)
 }
 
-func TestOutputJsonRawMsg(t *testing.T) {
-	type msgHolder struct {
-		Data    string
-		RawData json.RawMessage
+func TestMarshalForRawMsg01(t *testing.T) {
+	type dataHolder1 struct {
+		List interface{} `json:"list"`
+		Obj  interface{} `json:"obj"`
+	}
+	type dataHolder2 struct {
+		RawList json.RawMessage `json:"rawlist"`
+		RawObj  json.RawMessage `json:"rawobj"`
 	}
 
-	data := `{"data":"hello world"}`
-	holder := msgHolder{}
-	holder.Data = data
-	holder.RawData = json.RawMessage(data)
+	listStr := `[1,2,3]`
+	objStr := `{"data":"helloworld"}`
 
-	b, err := json.MarshalIndent(&holder, "", "  ")
+	dHolder1 := dataHolder1{
+		List: listStr,
+		Obj:  objStr,
+	}
+	b, err := json.Marshal(&dHolder1)
 	assert.NoError(t, err)
-	t.Logf("data:\n%s\n", b)
+	t.Logf("json results:\n%s", b)
+
+	dHolder2 := dataHolder2{
+		RawList: json.RawMessage(listStr),
+		RawObj:  json.RawMessage(objStr),
+	}
+	b, err = json.Marshal(&dHolder2)
+	assert.NoError(t, err)
+	t.Logf("json results:\n%s", b)
 }
 
-func TestMarshalForRawMsg(t *testing.T) {
+func TestMarshalForRawMsg02(t *testing.T) {
 	type base struct {
 		Content string `json:"content"`
 	}
@@ -152,7 +193,51 @@ func TestMarshalForRawMsg(t *testing.T) {
 	t.Logf("json results:\n%s", bytes)
 }
 
-func TestUnmarshalForRawMsg(t *testing.T) {
+func TestUnmarshalForRawMsg01(t *testing.T) {
+	type dataHolder1 struct {
+		List []int             `json:"list"`
+		Obj  map[string]string `json:"obj"`
+	}
+	type dataHolder2 struct {
+		RawList json.RawMessage `json:"list"`
+		RawObj  json.RawMessage `json:"obj"`
+	}
+
+	b := []byte(`{"list":[1,2,3],"obj":{"data":"helloworld"}}`)
+	dHolder1 := dataHolder1{}
+	err := json.Unmarshal(b, &dHolder1)
+	assert.NoError(t, err)
+	t.Logf("struct data: %+v, %+v", dHolder1.List, dHolder1.Obj)
+
+	dHolder2 := dataHolder2{}
+	err = json.Unmarshal(b, &dHolder2)
+	assert.NoError(t, err)
+	t.Logf("raw data: %s, %s", dHolder2.RawList, dHolder2.RawObj)
+
+	var tmp interface{}
+	err = json.Unmarshal(dHolder2.RawList, &tmp)
+	assert.NoError(t, err)
+	list, ok := tmp.([]interface{})
+	assert.True(t, ok)
+	t.Log("list data:", list)
+
+	var ints []int
+	err = json.Unmarshal(dHolder2.RawList, &ints)
+	assert.NoError(t, err)
+	t.Log("list ints:")
+	for i := 0; i < len(ints); i++ {
+		t.Logf("%d", ints[i])
+	}
+
+	tmp = nil
+	err = json.Unmarshal(dHolder2.RawObj, &tmp)
+	assert.NoError(t, err)
+	m, ok := tmp.(map[string]interface{})
+	assert.True(t, ok)
+	t.Log("object data:", m["data"])
+}
+
+func TestUnmarshalForRawMsg02(t *testing.T) {
 	type Color struct {
 		Type string `json:"type"`
 		// delay parsing until we know the color type
@@ -170,8 +255,8 @@ func TestUnmarshalForRawMsg(t *testing.T) {
 	}
 
 	b := []byte(`[
-    {"type": "YCbCr", "value":{"Y": 255, "Cb": 0, "Cr": -10}},
-    {"type": "RGB",  "value":{"R": 98, "G": 218, "B": 255}}
+    {"type":"YCbCr","value":{"Y":255,"Cb":0,"Cr":10}},
+    {"type":"RGB","value":{"R":98,"G":218,"B":255}}
 ]`)
 	colors := []Color{}
 	assert.NoError(t, json.Unmarshal(b, &colors))
@@ -311,7 +396,6 @@ func TestJsonDecode(t *testing.T) {
 	`
 
 	dec := json.NewDecoder(strings.NewReader(stream))
-	// dec.UseNumber()
 	t.Log("output:")
 	for {
 		var m Message
@@ -324,6 +408,80 @@ func TestJsonDecode(t *testing.T) {
 		}
 		t.Log(m.Name, m.Text)
 	}
+}
+
+func TestJsonDecodeUseNumber(t *testing.T) {
+	// 默认情况下，如果是 interface{} 类型对应数字的情况会序列化为 float64 类型，如果输入的数字比较大，这个表示会有损精度。
+	// 可以 UseNumber() 启用 json.Number 来用字符串表示数字。
+	decoder1 := json.NewDecoder(strings.NewReader("123"))
+	var obj1 interface{}
+	err := decoder1.Decode(&obj1)
+	assert.NoError(t, err)
+	t.Logf("kind:%s, value:%.2f", reflect.TypeOf(obj1).Kind().String(), obj1)
+
+	decoder2 := json.NewDecoder(strings.NewReader("123"))
+	decoder2.UseNumber()
+	var obj2 interface{}
+	err = decoder2.Decode(&obj2)
+	assert.NoError(t, err)
+	assert.Equal(t, json.Number("123"), obj2)
+	t.Logf("kind:%s, value:%s", reflect.TypeOf(obj2).Kind().String(), obj2)
+}
+
+func TestJsonMarshalForTemp(t *testing.T) {
+	// 临时忽略掉 password 字段，并且添加 token 字段
+	type User struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	user := User{
+		Email:    "foo@gmail.com",
+		Password: "foo",
+	}
+
+	b, err := json.Marshal(struct {
+		*User
+		Token    string `json:"token"`
+		Password bool   `json:"password,omitempty"`
+	}{
+		User:  &user,
+		Token: "test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("json result: %s", b)
+}
+
+// Json marshal for custom time format
+
+// 自定义类型并实现 MarshalJSON
+type timeImplMarshaler time.Time
+
+func (ti timeImplMarshaler) MarshalJSON() ([]byte, error) {
+	sec := time.Time(ti).Unix()
+	return []byte(strconv.FormatInt(sec, 10)), nil
+}
+
+func TestJsonMarshalForCustomTime(t *testing.T) {
+	type MyJob struct {
+		Name    string            `json:"name"`
+		Startat timeImplMarshaler `json:"start_at"`
+		Endat   time.Time         `json:"end_at"`
+	}
+
+	now := time.Now()
+	job := MyJob{
+		Name:    "setup",
+		Startat: timeImplMarshaler(now),
+		Endat:   now,
+	}
+
+	b, err := json.Marshal(job)
+	assert.NoError(t, err)
+	t.Logf("json result: %s", b)
 }
 
 //
