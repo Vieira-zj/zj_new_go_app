4ce0296b52a8b542172912a70fc6be1aa0b7bb45
zhengjin <zivieira@163.com>
2023-06-15 18:17:55 +0800

    Add chain demo.

---
+++ demo.go.new/go1_183_demos/demos/chain.go
@@ -0,0 +1,98 @@
+package demos
+
+import (
+	"context"
+	"fmt"
+)
+
+type Interceptor interface {
+	intercept(*MyInterceptorChain) error
+}
+
+// InterceptorChain
+
+type MyInterceptorChain struct {
+	index        int
+	ctx          context.Context
+	interceptors []Interceptor
+	errors       []error
+}
+
+func NewMyInterceptorChain(index int, interceptors []Interceptor) *MyInterceptorChain {
+	return &MyInterceptorChain{
+		index:        index,
+		ctx:          context.Background(),
+		interceptors: interceptors,
+		errors:       make([]error, 0),
+	}
+}
+
+func (chain *MyInterceptorChain) Proceed() {
+	if chain.index >= len(chain.interceptors) {
+		fmt.Println("end of chain")
+		return
+	}
+
+	interceptor := chain.interceptors[chain.index]
+	chain.index += 1
+	next := chain
+	if err := interceptor.intercept(next); err != nil {
+		chain.errors = append(chain.errors, err)
+	}
+}
+
+// Interceptors
+
+type KeyName string
+
+type MyInterceptorOne struct {
+	name string
+}
+
+func (i *MyInterceptorOne) intercept(next *MyInterceptorChain) error {
+	fmt.Println("MyInterceptorOne start:", i.name)
+	key := KeyName("name")
+	next.ctx = context.WithValue(next.ctx, key, "foo")
+
+	next.Proceed()
+
+	result := next.ctx.Value(key).(string)
+	fmt.Println("response:", result)
+
+	fmt.Println("MyInterceptorOne end:", i.name)
+	return nil
+}
+
+type MyInterceptorTwo struct {
+	name string
+}
+
+func (i *MyInterceptorTwo) intercept(next *MyInterceptorChain) error {
+	fmt.Println("MyInterceptorTwo start:", i.name)
+	key := KeyName("name")
+	name := next.ctx.Value(key).(string)
+	name = "hello " + name
+	next.ctx = context.WithValue(next.ctx, key, name)
+
+	next.Proceed()
+
+	fmt.Println("MyInterceptorTwo end:", i.name)
+	return nil
+}
+
+type MyInterceptorErr struct {
+	name    string
+	isError bool
+}
+
+func (i *MyInterceptorErr) intercept(next *MyInterceptorChain) error {
+	fmt.Println("MyInterceptorErr start:", i.name)
+	if i.isError {
+		return fmt.Errorf("mock error")
+	}
+
+	next.Proceed()
+
+	fmt.Println("MyInterceptorErr end:", i.name)
+	return nil
+}
---
+++ demo.go.new/go1_183_demos/demos/chain_test.go
@@ -0,0 +1,26 @@
+package demos
+
+import "testing"
+
+func TestMyInterceptorChain(t *testing.T) {
+	interceptors := []Interceptor{
+		&MyInterceptorOne{
+			name: "1st-interceptor",
+		},
+		&MyInterceptorErr{
+			name:    "err-interceptor",
+			isError: false,
+		},
+		&MyInterceptorTwo{
+			name: "2nd-interceptor",
+		},
+	}
+
+	chain := NewMyInterceptorChain(0, interceptors)
+	chain.Proceed()
+
+	for _, err := range chain.errors {
+		t.Log("chain error:", err)
+	}
+	t.Log("chain done")
+}
